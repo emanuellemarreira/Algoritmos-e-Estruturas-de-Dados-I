0dbcf835a704582941a7d528a31bb827c8333147
Emanuelle <emanuellemarreira@gmail.com>
2023-06-10 20:22:56 -0300

    Update llest2_main.c

--- 20230602/20230602002/llest2_main.c
+++ 20230602/20230602002/llest2_main.c
@@ -1,25 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "llest2.h"
+#include "llest2ok.h"
 int main(int argc, char *argv[]) {
     Lista l, la, lb;
-	l.inicio = NULL;
-	l.fim = NULL;
-	inserirFim(&l,1);
-	inserirFim(&l,2);
-	inserirFim(&l,3);
-	inserirFim(&l,4);
-	printf("\nLISTA 1\n");
-	imprimir(&l);
-	la.inicio = NULL;
-	la.fim = NULL;
-	a(&l, &la);
-	printf("\n\nLISTA 2\n");
-	imprimir(&la);
-	lb.inicio = NULL;
-	lb.fim = NULL;
-	b(&l, &lb);
-	printf("\n\nLISTA 3\n");
-	imprimir(&lb);	
-	return 0;
+    inicializar(&l);
+    inserirFim(&l, 1);
+    inserirFim(&l, 2);
+    inserirFim(&l, 3);
+    inserirFim(&l, 4);
+    printf("\nLISTA 1\n");
+    imprimir(&l);
+    inicializar(&la);
+    a(&l, &la);
+    printf("\n\nLISTA 2\n");
+    imprimir(&la);
+    inicializar(&lb);
+    b(&l, &lb);
+    printf("\n\nLISTA 3\n");
+    imprimir(&lb);
+    return 0;
 }
