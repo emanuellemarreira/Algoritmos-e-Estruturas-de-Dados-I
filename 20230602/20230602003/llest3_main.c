0bfdec04b55a051cb51ba3d0f4914d82b6b0fc3a
Emanuelle <emanuellemarreira@gmail.com>
2023-06-10 22:19:28 -0300

    Update llest3_main.c

--- 20230602/20230602003/llest3_main.c
+++ 20230602/20230602003/llest3_main.c
@@ -1,39 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define "llest3.h"
+#include "llest3ok.h"
 int main(int argc, char *argv[]) {
-	Lista l, la, lb;
-	l.inicio = NULL;
-	l.fim = NULL;
-	inserirFim(&l,1);
-	inserirFim(&l,2);
-	inserirFim(&l,3);
-	inserirFim(&l,4);
-	printf("\nLISTA 1\n");
-	imprimir(&l);
-	la.inicio = NULL;
-	la.fim = NULL;
-	copiarlista(&l, &la);
-	printf("\n\nLISTA 2\n");
-	imprimir(&la);
-	la.inicio = NULL;//meio que pra esvaziar a lista
-	la.fim = NULL;
-	inserirFim(&la,5);
-	inserirFim(&la,6);
-	concatenar(&l,&la);
-	printf("\n\nLISTA 1 concatenada\n");
-	imprimir(&l);
-	la.inicio = NULL;//meio que pra esvaziar a lista
-	la.fim = NULL;
-	inserirFim(&la,11);
-	inserirFim(&la,22);
-	inserirFim(&la,33);
-	inserirFim(&la,44);
-	inserirFim(&la,55);
-	lb.inicio = NULL;
-	lb.fim = NULL;
-	printf("\n\nLISTA 1 INTERCALADA\n");
-	intercalar(&l,&la,&lb);
-	imprimir(&lb);
-	return 0;
+    Lista l, la, lb;
+    inicializar(&l);
+    inserirFim(&l, 1);
+    inserirFim(&l, 2);
+    inserirFim(&l, 3);
+    inserirFim(&l, 4);
+    printf("\nLISTA 1\n");
+    imprimir(&l);
+    inicializar(&la);
+    printf("\n\nLISTA 1 - copia l2\n");
+    copiarLista(&l, &la);
+    imprimir(&la);
+    printf("\n\nLISTA 1 concatenada com l2\n");
+    concatenarLista(&l, &la);
+    imprimir(&l);
+    inicializar(&lb);
+    printf("\n\nl1 intercalada com l2\n");
+    lb = intercalarListas(&l,&la);
+    imprimir(&lb);
+
+    return 0;
 }
