5f98f61cf07e6f0632cbfe3e67dea00edc3f65fe
Emanuelle <emanuellemarreira@gmail.com>
2023-06-16 01:02:59 -0300

    Update listalprak.h

--- 20230612/20230612001/listalprak.h
+++ 20230612/20230612001/listalprak.h
@@ -12,10 +12,11 @@ typedef struct {
 } Lista;
 void inicializar_lista(Lista* lista);
 void inserir_elemento(Lista* lista, int valor);
+void inserir_emk(Lista* lista, No* inserir);
 int buscar_maior_elemento(Lista* lista);
-void remover_elemento(Lista* lista, No* no);
+No* buscaElem_e_Pred(Lista* m, int valor, No** pred);
+No* remover_elemento(Lista* lista, int valor) {
 void armazenar_maior_elemento(Lista* L, Lista* K);
 void imprimir_lista(Lista* lista);
 
 #endif
-
